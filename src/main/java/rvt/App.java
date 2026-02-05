24c9f8811879d1518746e525984339490ac5904f
24DP3LStra <A240886LS@rvt.lv>
2026-02-05 12:16:22 +0000

    Unfinished lesson work

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -9,14 +9,21 @@ public class App {
 
     public static void main(String[] args) {
         File file = new File("data/todo.csv");
-        try {
-            Scanner reader = new Scanner(file);
-            System.out.println(
-                reader.nextLine()
-            );
+        try (Scanner reader = new Scanner(new File("data/data.csv"))){
+            reader.nextLine();
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                String[] parts = row.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                int id = Integer.valueOf(parts[2]);
+                String email = parts[3];
+                String course = parts[4];
+                double gpa = Double.valueOf(parts[5]);
+            }
         }
         catch (FileNotFoundException e){
-
+            System.out.println("Error" + e.getMessage());
         }
 
     }
--- src/main/java/rvt/TodoList.java
+++ src/main/java/rvt/TodoList.java
@@ -7,20 +7,37 @@ import java.util.Scanner;
 
 public class TodoList {
     private ArrayList tasks;
+    private final String filePath = "data/todo.csv";
 
     public TodoList() {
         this.tasks = new ArrayList<>();
+
+    }
+
+    private void loadFromFile(){
+
     }
 
     public void add(String task) {
         tasks.add(task);
     }
 
+    // private boolean updateFile(){
+
+    // }
+
+
+
+
     public void print() {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + ": " + tasks.get(i));
         }
     }
+
+    // private int getLastId(){
+
+    // }
 
     public void remove(int number) {
         // number is 1-based, ArrayList is 0-based
