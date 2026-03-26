d59ad951be1ca9fbbc916bfcd1158fa0fc4b91f6
24DP3LStra <A240886LS@rvt.lv>
2026-03-26 12:43:48 +0200

    Start of console colors work (UNFINISHED)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,21 +2,16 @@ package rvt;
 
 public class App {
     public static void main(String[] args) {
-        TodoList todoList = new TodoList();
-        todoList.add("Buy groceries");
-        todoList.add("Finish homework");
-        todoList.add("Call mom");
+        System.out.println("\u001B[34m"
+            + "Hello Colored console"
+            + "\u001B[0m"
+        );
+        System.out.println("RVT!");
 
-        todoList.print();
-        todoList.remove(2); // Remove the second task
-        System.out.println("After removing task 2:");
-        todoList.print();
-        todoList.updateFile(1, "Buy groceries and cook dinner"); // Update the first task
-        System.out.println("After updating task 1:");
-        todoList.print();
-        todoList.add("Go for a run");
-        System.out.println("After adding a new task:");
-        todoList.print(); 
-        todoList.checkEventString("run"); // Check if "run" is in any task
+    }
+    public static void setPractice(MansEnum group){
+        if (group == MansEnum.DP2_1){
+            String strGroup = MansEnum.DP2_1.value;
+        }
     }
 }
---
+++ src/main/java/rvt/ConsoleColors.java
@@ -0,0 +1,7 @@
+package rvt;
+
+public enum ConsoleColors {
+    RESET ("\u001B[0m"),
+    RED ("\u001B[31m"),
+    
+}
