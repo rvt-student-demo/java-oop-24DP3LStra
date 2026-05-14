f74ad9b2bd1b4eaae39b3ba089fcaa747b0e556b
24DP3LStra <A240886LS@rvt.lv>
2026-05-14 15:43:33 +0300

    XML stundas uzdevuma izpilde, viss strādā

--- src/main/java/rvt/ConsoleColors.java
+++ src/main/java/rvt/ConsoleColors.java
@@ -2,6 +2,15 @@ package rvt;
 
 public enum ConsoleColors {
     RESET ("\u001B[0m"),
-    RED ("\u001B[31m"),
-    
+    RED ("\u001B[31m");
+
+    private final String code;
+
+    ConsoleColors(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
